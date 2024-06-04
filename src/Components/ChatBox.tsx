bdca97bf98cae43f9190306aed0493867e1290f2
Anuoluwapo <anuvictor2005@gmail.com>
2024-06-04 13:12:18 +0100

    changes made

--- src/Components/ChatBox.tsx
+++ src/Components/ChatBox.tsx
@@ -1,18 +1,30 @@
 import { Box, Button, Image, TextInput } from "@mantine/core";
 import emojiIcon from "../assets/emojiButtonIcon.svg";
+import data from "@emoji-mart/data";
+import Picker from "@emoji-mart/react";
 import { useState } from "react";
 
 const ChatBox = () => {
   const [myMessage, setMyMessage] = useState("");
   const [showEmojis, setShowEmojis] = useState(false);
+
   const handleShowEmojis = () => {
     setShowEmojis(!showEmojis);
   };
-  const handleMyMessage = (myMessage: string) => {
-    setMyMessage(myMessage);
+
+  const handleMyMessage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMyMessage(event.target.value);
   };
+
+  const handleEmojiSelect = (emoji: any) => {
+    setMyMessage((prevMessage) => prevMessage + emoji.native);
+  };
+
   return (
     <Box w="70%" pos="fixed">
+      <Box pos="absolute" top="-420px" left="0">
+        {showEmojis && <Picker data={data} onEmojiSelect={handleEmojiSelect} />}
+      </Box>
       <TextInput
         w="auto"
         placeholder="Send a message"
@@ -21,7 +33,10 @@ const ChatBox = () => {
             src={emojiIcon}
             w="1.5rem"
             h="1.5rem"
-            onClick={() => handleShowEmojis}
+            mr="md"
+            ml="md"
+            onClick={handleShowEmojis}
+            style={{ cursor: "pointer" }}
           />
         }
         rightSection={
@@ -30,9 +45,10 @@ const ChatBox = () => {
           </Button>
         }
         value={myMessage}
-        onChange={() => handleMyMessage(myMessage)}
+        onChange={handleMyMessage}
       />
     </Box>
   );
 };
+
 export default ChatBox;
